b43e79ac3076857f5957108437cce4bc186d0a14
khalil saidane <khalilsaidane@khalils-MacBook-Pro.local>
2020-02-24 20:45:28 +0100

    search restaurant by name

--- profiles/views.py
+++ profiles/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from django.contrib.auth import get_user_model
 from django.http import Http404
+from restaurents.models import Restaurant
+from menus.models import Menu
 
 User = get_user_model()
 
@@ -17,3 +19,17 @@ class ProfileDetailView(DetailView):
         if username is None:
             raise Http404
         return get_object_or_404(User, username__iexact=username, is_active=True)
+
+    def get_context_data(self,*args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        user = context['user']
+        query = self.request.GET.get('q')
+        menu_exists = Menu.objects.filter(user=user).exists()
+        qs = Restaurant.objects.filter(user=user)
+        print(query)
+        if query:
+            qs = qs.filter(name__icontains=query)
+            print(qs.count())
+        if qs.exists() and menu_exists:
+            context['restaurants'] = qs
+        return context
